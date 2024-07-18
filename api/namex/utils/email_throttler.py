dfbab96ecf9ea3f9ef8b9892abbddd68abd204d3
EPortman <Ethan.Portman@gov.bc.ca>
2024-07-17 20:50:55 -0700

    ANother checkpoint

--- api/namex/utils/email_throttler.py
+++ api/namex/utils/email_throttler.py
@@ -1,6 +1,7 @@
 import threading
 import asyncio
 from typing import Callable
+from namex.models import PendingEmail
 
 class EmailThrottler:
     """
@@ -8,15 +9,14 @@ class EmailThrottler:
     manages an event loop where email tasks are scheduled and awaited for each NR. Once a task completes
     on the emailer thread, the task makes a call to send the most recent email stored for the NR. 
 
-    This thread is created on start-up and each WSGI worker gets one of these. 
+    All emails stored by this class are written to and deleted from disk as they are used to prevent
+    losing emails if the server shuts down unexpectedly. 
     """
 
     def __init__(self, app):
         self.app = app
         self.throttle_time = 10
         self.email_thread_status = threading.Event()
-        self.email_thread_ready_to_close = threading.Event()
-        self.email_thread_finished = threading.Event()
         try:
             self.email_thread = threading.Thread(target=self.start_event_loop, daemon=True)
             self.email_thread_loop = asyncio.new_event_loop()
@@ -29,13 +29,13 @@ class EmailThrottler:
 
         except Exception as e:
             self.app.logger.error(f'Failed to Initialize Email Throttler, {e}')
+            self.email_thread_status.clear()
 
 
     def start_event_loop(self):
         """
         Starts the event loop that runs indefinetly on the emailer thread. Email tasks from the
-        main thread are scheduled using this loop. If an exception occurs, all of the emails 
-        are flushed and the loop is stopped and emailer thread closed.
+        main thread are scheduled using this loop. 
         """
         try:
             asyncio.set_event_loop(self.email_thread_loop)
@@ -43,21 +43,22 @@ class EmailThrottler:
         except Exception as e:
             self.app.logger.error(f'Exception in Email Throttler Loop: {e}')
         finally:
-            self.email_thread_ready_to_close.wait()
+            self.app.logger.debug('Uh oh, Email Throttler Loop has stopped unexpectedly.')
             self.email_thread_status.clear()
-            self.email_thread_loop.close()
-            self.email_thread_finished.set()
 
 
-    def schedule_or_update_email_task(self, nr_num: str, email: Callable):
+    def schedule_or_update_email_task(self, nr_num: str, decision: str, email: Callable):
         """
         Schedules a new email task on the emailer thread or updates an existing task's email callback.
         Once the task completes only the most recent email for the NR will be sent.
 
         If an error is encountered all of the pending emails are sent and the emailer thread is closed.
         """
+        # Update Email in Memory and in db
+        self.email_callbacks[nr_num] = email
+        PendingEmail.add_or_update_email(nr_num, decision)
+
         try:
-            self.email_callbacks[nr_num] = email
 
             if nr_num not in self.email_tasks.keys():
                 self.email_thread_loop.call_soon_threadsafe(
@@ -65,12 +66,9 @@ class EmailThrottler:
                         {nr_num: asyncio.create_task(self.send_email_after_delay(nr_num))}
                     )
                 )
-
         except Exception as e:
             self.app.logger.error(f'Exception when throttling the email: {e}')
-            future = asyncio.run_coroutine_threadsafe(self.flush_and_stop_emailer_thread(), self.email_thread_loop)
-            future.result()
-            self.email_thread_ready_to_close.set()
+            email()
 
 
     async def send_email_after_delay(self, nr_num: str):
@@ -83,28 +81,11 @@ class EmailThrottler:
         with self.app.app_context():
             try:
                 self.email_callbacks[nr_num]()
-                self.email_tasks.pop(nr_num, None)
-                self.email_callbacks.pop(nr_num, None)
+                PendingEmail.delete_record(nr_num)
             except Exception as e:
                 self.app.logger.error(f'Error handling email for {nr_num}: {e}')
+            finally:
+                self.email_tasks.pop(nr_num, None)
+                self.email_callbacks.pop(nr_num, None)
 
 
-    async def flush_and_stop_emailer_thread(self):
-        """
-        Sends all emails and prepares the emailer thread for closure. 
-        """
-        with self.app.app_context():
-            # Cancel all tasks
-            for task in self.email_tasks.values():
-                task.cancel()
-            await asyncio.gather(*self.email_tasks.values(), return_exceptions=True)
-            self.email_tasks.clear()
-            
-            # Execute any pending callbacks
-            for callback in self.email_callbacks.values():
-                callback()
-            self.email_callbacks.clear()
-
-            # Stop the loop
-            self.email_thread_loop.stop()
-            self.email_thread_ready_to_close.set()
--- api/namex/utils/queue_util.py
+++ api/namex/utils/queue_util.py
@@ -44,7 +44,7 @@ def throttle_email_notification(nr_num: str, decision: str):
 
     if email_throttler.email_thread_status.is_set():
         current_app.logger.debug('Throttling email for %s with decision %s', nr_num, decision)
-        email_throttler.schedule_or_update_email_task(nr_num, email_callback)
+        email_throttler.schedule_or_update_email_task(nr_num, decision, email_callback)
     else:
         current_app.logger.debug('Email throttler is not available. Publishing email immediately.')
         email_callback()
